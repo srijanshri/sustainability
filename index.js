ebb4c879ffab1fba2c1439716af692082e79fb22
srijanshri <58974370+srijanshri@users.noreply.github.com>
2021-08-11 02:31:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+const body = document.body;
+const content = document.querySelector('.js-content');
+const blocks = document.querySelectorAll('.block');
+
+const updateOffset = () => {
+  requestAnimationFrame(updateOffset);
+  body.style.setProperty('--y', content.scrollTop);
+  updateProps();
+};
+
+const updateProps = () => {
+  let i = -1;
+  for (let block of blocks) {
+    i += 1;
+    let top = blocks[i].getBoundingClientRect().top;
+    if (top < window.innerHeight * 1.3 && top > window.innerHeight * -1.3) {
+      body.style.setProperty(`--yBlock-${i + 1}`, top);
+    } else {
+      body.style.setProperty(`--yBlock-${i + 1}`, 0);
+    }
+  }
+};
+
+updateProps();
+updateOffset();
+
+var animateButton = function(e) {
+
+  e.preventDefault;
+  //reset animation
+  e.target.classList.remove('animate');
+  
+  e.target.classList.add('animate');
+  setTimeout(function(){
+    e.target.classList.remove('animate');
+  },700);
+};
+
+var bubblyButtons = document.getElementsByClassName("bubbly-button");
+
+for (var i = 0; i < bubblyButtons.length; i++) {
+  bubblyButtons[i].addEventListener('click', animateButton, false);
+}
